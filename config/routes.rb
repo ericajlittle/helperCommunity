456a7769374cd55389e7438ccd3e42b668900333
MichaelMini <mcho911@gmail.com>
2017-02-24 21:01:36 +0000

    added sessions and user

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,11 @@ Rails.application.routes.draw do
 
   resources :users
 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
 end
