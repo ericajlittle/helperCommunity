2f52dfe84e53777c5040e34285bcdb1761cae383
MichaelMini <mcho911@gmail.com>
2017-03-01 03:25:03 +0000

    created reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,17 +1,20 @@
 class ReviewsController < ApplicationController
   before_filter :authorize
   def create
-    @user = User.find(params[:user_id])
-    # @review = @user.reviews.create(review_params)
-    # redirect_to user_path(@user)
-    @review = @user.reviews.new(review_params) # I need to know how this works???
-    # @review.user_id = current_user.id if current_user
-    @review.user_id = @user.id
-    if @review.save!
-      redirect_to @review.user
-    else
-      redirect :back
-    end # ???
+    if current_user
+      @user = User.find(params[:user_id])
+      # @review = @user.reviews.create(review_params)
+      # redirect_to user_path(@user)
+      @review = @user.reviews.new(review_params) # I need to know how this works???
+      # @review.user_id = current_user.id if current_user
+      @review.user_id = @user.id
+      @review.reviewer_id = current_user.id
+      if @review.save!
+        redirect_to @review.user
+      else
+        redirect :back
+      end # ???
+    end
   end
 
   def destroy
