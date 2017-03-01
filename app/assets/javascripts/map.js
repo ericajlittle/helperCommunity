7ef1963de12f0e63c15bffd2a0fec8785b5cfff4
Irene <carina.omsk@gmail.com>
2017-03-01 22:54:07 +0000

    Tried to implement map resize

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,46 +1,53 @@
-function initMap(data) {
-  var uluru = {lat: 49.2821004, lng: -123.1082745};
-
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 12,
-    center: uluru
-  });
-
-  var contentString;
-
-  var infowindow = new google.maps.InfoWindow({
-    content: contentString,
-    maxWidth: 300
-  });
-
-  if (data) {
-    for(i = 0; i < data.length; i++) {
-      var marker = new google.maps.Marker({
-        position: {lat: data[i]["lat"], lng: data[i]["lng"]},
-        map: map
-      });
-
-      contentString = '<div class="event-title">' +
-                      '<a href = "/events/' + data[i]['id'] + '">' + data[i]['title'] + '</a>' +
-                      '<p>' + data[i]['description'] + '</p>'
-                      '</div>';
-
-      makeInfoWindowEvent(map, infowindow, contentString, marker);
-  }
-  }
-}
-
-function makeInfoWindowEvent(map, infowindow, contentString, marker) {
-  google.maps.event.addListener(marker, 'click', function() {
-    infowindow.setContent(contentString);
-    infowindow.open(map, marker);
-  });
-}
-
 $(function() {
+  function initMap(data) {
+    var uluru = {lat: 49.2821004, lng: -123.1082745};
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 12,
+      center: uluru
+    });
+
+    var contentString;
+
+    var infowindow = new google.maps.InfoWindow({
+      content: contentString,
+      maxWidth: 300
+    });
+
+    if (data) {
+      for(i = 0; i < data.length; i++) {
+        var marker = new google.maps.Marker({
+          position: {lat: data[i]["lat"], lng: data[i]["lng"]},
+          map: map
+        });
+
+        contentString = '<div class="event-title">' +
+                        '<a href = "/events/' + data[i]['id'] + '">' + data[i]['title'] + '</a>' +
+                        '<p>' + data[i]['description'] + '</p>'
+                        '</div>';
+
+        makeInfoWindowEvent(map, infowindow, contentString, marker);
+      }
+    }
+  }
+
+  function makeInfoWindowEvent(map, infowindow, contentString, marker) {
+    google.maps.event.addListener(marker, 'click', function() {
+      infowindow.setContent(contentString);
+      infowindow.open(map, marker);
+    });
+  }
+
+  $(window).resize(function() {
+    var height = $(window).height() - $('nav').height() - $('.index-map .container').height();
+    $("#map").css('height', height);
+    google.maps.event.trigger(map,'resize')
+    console.log(height);
+  });
 
   var height = $(window).height() - $('nav').height() - $('.index-map .container').height();
   $("#map").css('height', height);
+  console.log(height);
 
   $.ajax({
     url: "/events",
