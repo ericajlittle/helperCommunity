45bd8d68ecf889cd53529cd4be4c09f158f99696
MichaelMini <mcho911@gmail.com>
2017-03-02 02:52:45 +0000

    moved map.js into map.jsx

--- client/app/bundles/HelperApp/components/Map.jsx
+++ client/app/bundles/HelperApp/components/Map.jsx
@@ -9,6 +9,53 @@ export default class Map extends React.Component {
     this.state = { name: this.props.name };
   }
 
+  componentDidMount() {
+    console.log('getting events');
+    this.getEvents();
+  }
+
+  getEvents() {
+    const _this = this;
+    $.ajax({
+      url: "/events",
+      dataType: "json"
+    }).done(function(data) {
+      _this.initMap(data);
+    });
+  }
+
+  initMap(data) {
+    var uluru = {lat: 49.2821004, lng: -123.1082745};
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 12,
+      center: uluru
+    });
+
+    var contentString;
+
+    var infowindow = new google.maps.InfoWindow({
+      content: contentString,
+      maxWidth: 300
+    });
+
+    if (data) {
+      for( let i = 0; i < data.length; i++) {
+        var marker = new google.maps.Marker({
+          position: {lat: data[i]["lat"], lng: data[i]["lng"]},
+          map: map
+        });
+
+        contentString = '<div class="event-title">' +
+                        '<a href = "/events/' + data[i]['id'] + '">' + data[i]['title'] + '</a>' +
+                        '<p>' + data[i]['description'] + '</p>'
+                        '</div>';
+
+        makeInfoWindowEvent(map, infowindow, contentString, marker);
+    }
+    }
+  }
+
   render() {
     console.log('Loading Map component');
     return (
