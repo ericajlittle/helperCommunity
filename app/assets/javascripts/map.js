7e35f59a2ea3d12adb072bf6340b6cf266f997dc
MichaelMini <mcho911@gmail.com>
2017-03-02 21:08:12 +0000

    Testing two-points map is working. Working on new database table and make it to work.

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,48 +1,103 @@
-function initMap(data) {
-  var uluru = {lat: 49.2821004, lng: -123.1082745};
+// function initMap(data) {
+//   var uluru = {lat: 49.2821004, lng: -123.1082745};
 
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 12,
-    center: uluru
-  });
+//   var map = new google.maps.Map(document.getElementById('map'), {
+//     zoom: 12,
+//     center: uluru
+//   });
 
-  var contentString;
+//   var contentString;
 
-  var infowindow = new google.maps.InfoWindow({
-    content: contentString,
-    maxWidth: 300
-  });
+//   var infowindow = new google.maps.InfoWindow({
+//     content: contentString,
+//     maxWidth: 300
+//   });
 
-  if (data) {
-    for(i = 0; i < data.length; i++) {
-      var marker = new google.maps.Marker({
-        position: {lat: data[i]["lat"], lng: data[i]["lng"]},
-        map: map
-      });
+//   if (data) {
+//     for(i = 0; i < data.length; i++) {
+//       var marker = new google.maps.Marker({
+//         position: {lat: data[i]["lat"], lng: data[i]["lng"]},
+//         map: map
+//       }),
+//       markerB = new google.maps.Marker({
+//         position: {lat: 49.2821004, lng: -123.1082745},
+//         title: "point B",
+//         label: "B",
+//         map: map
+//       });
 
-      contentString = '<div class="event-title">' +
-                      '<a href = "/events/' + data[i]['id'] + '">' + data[i]['title'] + '</a>' +
-                      '<p>' + data[i]['description'] + '</p>'
-                      '</div>';
+//       contentString = '<div class="event-title">' +
+//                       '<a href = "/events/' + data[i]['id'] + '">' + data[i]['title'] + '</a>' +
+//                       '<p>' + data[i]['description'] + '</p>'
+//                       '</div>';
 
-      makeInfoWindowEvent(map, infowindow, contentString, marker);
-  }
-  }
+//       makeInfoWindowEvent(map, infowindow, contentString, marker, markerB);
+//   }
+//   }
+// }
+
+// function makeInfoWindowEvent(map, infowindow, contentString, marker) {
+//   google.maps.event.addListener(marker, 'click', function() {
+//     infowindow.setContent(contentString);
+//     infowindow.open(map, marker);
+//   });
+// }
+
+// // $(function() {
+// //   $.ajax({
+// //     url: "/events",
+// //     dataType: "json"
+// //   }).done(function(data) {
+// //     initMap(data);
+// //   });
+// // });
+
+function initMap() {
+    var pointA = {lat: 49.28126719999999, lng: -123.0769687},
+        pointB = {lat: 49.2821004, lng: -123.1082745},
+        myOptions = {
+            zoom: 7,
+            center: pointA
+        },
+        map = new google.maps.Map(document.getElementById('map'), myOptions),
+        // Instantiate a directions service.
+        directionsService = new google.maps.DirectionsService,
+        directionsDisplay = new google.maps.DirectionsRenderer({
+            map: map
+        }),
+        markerA = new google.maps.Marker({
+            position: pointA,
+            title: "point A",
+            label: "A",
+            map: map
+        }),
+        markerB = new google.maps.Marker({
+            position: pointB,
+            title: "point B",
+            label: "B",
+            map: map
+        });
+
+    // get route from A to B
+    calculateAndDisplayRoute(directionsService, directionsDisplay, pointA, pointB);
+
 }
 
-function makeInfoWindowEvent(map, infowindow, contentString, marker) {
-  google.maps.event.addListener(marker, 'click', function() {
-    infowindow.setContent(contentString);
-    infowindow.open(map, marker);
-  });
+
+
+function calculateAndDisplayRoute(directionsService, directionsDisplay, pointA, pointB) {
+    directionsService.route({
+        origin: pointA,
+        destination: pointB,
+        avoidTolls: true,
+        avoidHighways: false,
+        travelMode: google.maps.TravelMode.DRIVING
+    }, function (response, status) {
+        if (status == google.maps.DirectionsStatus.OK) {
+            directionsDisplay.setDirections(response);
+        } else {
+            window.alert('Directions request failed due to ' + status);
+        }
+    });
 }
-
-$(function() {
-  $.ajax({
-    url: "/events",
-    dataType: "json"
-  }).done(function(data) {
-    initMap(data);
-  });
-});
-
+// initMap();
