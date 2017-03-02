b199fd52c0aa76132512fa3e088247fdc1c19240
edwardxywang@gmail.com <edwardxywang@gmail.com>
2017-03-02 01:22:07 +0000

    clean some extra routes about rooms

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'rooms/show'
-
-  get 'rooms/create'
 
   get 'hello_world', to: 'hello_world#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
