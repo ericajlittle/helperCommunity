cdc6b45a4a2407dfedc18c99356cb1af7969328b
MichaelMini <mcho911@gmail.com>
2017-03-03 01:50:52 +0000

    Add def geocode_endpoints to convert two address into coordinates.

--- app/models/event.rb
+++ app/models/event.rb
@@ -9,8 +9,33 @@ class Event < ApplicationRecord
   # validates :scheduled_at, presence: true
   # validates :location, presence: true
 
-  geocoded_by :address, :latitude  => :lat, :longitude => :lng
+  # geocoded_by :address, :latitude  => :lat, :longitude => :lng
+  # geocoded_by :end_address, latitude: :end_lat, longitude: :end_lng
 
-  after_validation :geocode
+  # after_validation :geocode
+
+  before_save :geocode_endpoints
+
+private
+  #To enable Geocoder to works with multiple locations
+  def geocode_endpoints
+    if self.address_changed?
+      # if implement event-editing, next line must only run when from-address changes
+      geocoded = Geocoder.search(self.address).first
+      if geocoded
+        self.lat = geocoded.latitude
+        self.lng = geocoded.longitude
+      end
+    end
+    # Repeat for destination
+    if self.end_address_changed?
+      # if implement event-editing, next line must only run when to-address changes
+      geocoded = Geocoder.search(self.end_address).first
+      if geocoded
+        self.end_lat = geocoded.latitude
+        self.end_lng = geocoded.longitude
+      end
+    end
+  end
 
 end
