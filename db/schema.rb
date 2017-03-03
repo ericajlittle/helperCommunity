15c4508b691eb4e8c83d24802996fa7b5ce961ab
MichaelMini <mcho911@gmail.com>
2017-03-03 01:55:53 +0000

    Generated new Events column :end_address :end_lat :end_lng.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170301023906) do
+ActiveRecord::Schema.define(version: 20170303003613) do
 
   create_table "events", force: :cascade do |t|
     t.string   "title"
@@ -24,6 +24,9 @@ ActiveRecord::Schema.define(version: 20170301023906) do
     t.datetime "scheduled_at"
     t.string   "address"
     t.string   "city"
+    t.string   "end_address"
+    t.float    "end_lat"
+    t.float    "end_lng"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
