f91533207e6682c75e978897127383878b461d75
MichaelMini <mcho911@gmail.com>
2017-03-03 23:03:38 +0000

    Refactor markerA and markerB.

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,48 +1,3 @@
-// function initMap(data) {
-//   var uluru = {lat: 49.2821004, lng: -123.1082745};
-
-//   var map = new google.maps.Map(document.getElementById('map'), {
-//     zoom: 12,
-//     center: uluru
-//   });
-
-//   var contentString;
-
-//   var infowindow = new google.maps.InfoWindow({
-//     content: contentString,
-//     maxWidth: 300
-//   });
-
-//   if (data) {
-//     for(i = 0; i < data.length; i++) {
-//       var marker = new google.maps.Marker({
-//         position: {lat: data[i]["lat"], lng: data[i]["lng"]},
-//         map: map
-//       }),
-//       markerB = new google.maps.Marker({
-//         position: {lat: 49.2821004, lng: -123.1082745},
-//         title: "point B",
-//         label: "B",
-//         map: map
-//       });
-
-//       contentString = '<div class="event-title">' +
-//                       '<a href = "/events/' + data[i]['id'] + '">' + data[i]['title'] + '</a>' +
-//                       '<p>' + data[i]['description'] + '</p>'
-//                       '</div>';
-
-//       makeInfoWindowEvent(map, infowindow, contentString, marker, markerB);
-//     }
-//   }
-// }
-
-// function makeInfoWindowEvent(map, infowindow, contentString, marker) {
-//   google.maps.event.addListener(marker, 'click', function() {
-//     infowindow.setContent(contentString);
-//     infowindow.open(map, marker);
-//   });
-// }
-
 $(function() {
   $.ajax({
     url: "/events",
@@ -52,67 +7,6 @@ $(function() {
   });
 });
 
-// function initMap() {
-//     var pointA = {lat: 49.28126719999999, lng: -123.0769687},
-//         pointB = {lat: 49.2821004, lng: -123.1082745},
-//         myOptions = {
-//             zoom: 7,
-//             center: pointA
-//         },
-//         map = new google.maps.Map(document.getElementById('map'), myOptions),
-//         // Instantiate a directions service.
-//         directionsService = new google.maps.DirectionsService,
-//         directionsDisplay = new google.maps.DirectionsRenderer({
-//             map: map
-//         }),
-//         markerA = new google.maps.Marker({
-//             position: pointA,
-//             title: "point A",
-//             label: "A",
-//             map: map
-//         }),
-//         markerB = new google.maps.Marker({
-//             position: pointB,
-//             title: "point B",
-//             label: "B",
-//             map: map
-//         });
-
-//     // get route from A to B
-//     calculateAndDisplayRoute(directionsService, directionsDisplay, pointA, pointB);
-
-// }
-
-
-
-// function calculateAndDisplayRoute(directionsService, directionsDisplay, pointA, pointB) {
-//     directionsService.route({
-//         origin: pointA,
-//         destination: pointB,
-//         avoidTolls: true,
-//         avoidHighways: false,
-//         travelMode: google.maps.TravelMode.DRIVING
-//     }, function (response, status) {
-//         if (status == google.maps.DirectionsStatus.OK) {
-//             directionsDisplay.setDirections(response);
-//         } else {
-//             window.alert('Directions request failed due to ' + status);
-//         }
-//     });
-// }
-// initMap();
-// var geocoder = new google.maps.Geocoder();
-// var address = '1600 Pennsylvania Avenue NW Washington, DC 20500';
-
-// geocoder.geocode( { 'address': address}, function(results, status) {
-
-//     var latitude = results[0].geometry.location.lat();
-//     var longitude = results[0].geometry.location.lng();
-//     jQuery('#coordinates').val(latitude+', '+longitude);
-// console.log(latitude, longitude);
-
-// });
-// geocoder.geocode();
 
 
 function initMap(data) {
@@ -122,70 +16,82 @@ function initMap(data) {
     center: uluru
   });
 
-  var contentString;
+  if (data) {
+    for(i = 0; i < data.length; i++) {
 
+      addEventToMap(map, data[i]);
+
+    }
+  }
+}
+
+function requestDirections(map, start, end) {
+  var directionsService = new google.maps.DirectionsService;
+  directionsService.route({
+    origin: start,
+    destination: end,
+    travelMode: google.maps.DirectionsTravelMode.DRIVING
+  }, function(result) {
+    renderDirections(map, result);
+  });
+}
+
+function renderDirections(map, result) {
+  var directionsRenderer = new google.maps.DirectionsRenderer({
+    polylineOptions: {
+      strokeColor: "red"
+    },
+    suppressMarkers:true
+  });
+  directionsRenderer.setMap(map);
+  directionsRenderer.setDirections(result);
+}
+
+function addEventToMap(map, event) {
+  // make Markers A and B
+  // make info windows for both markers with the content they need
+  // set up click event listeners to open info windows
+  // request/render directions
+  //
+  var markerA = addMarker(map, event, event.lat, event.lng, true);
+  var markerB = addMarker(map, event, event.end_lat, event.end_lng, false);
+
+  requestDirections(map,
+    {lat: event["lat"], lng: event["lng"]},
+    {lat: event["end_lat"], lng: event["end_lng"]});
+
+  // makeInfoWindowEvent(map, infowindow, contentString, markerA, markerB);
+}
+
+function addMarker(map, event, lat, lng, isOrigin) {
+  var marker = new google.maps.Marker({
+    position: {lat, lng},
+    title: isOrigin ? "Origin" : "Destination",
+    label: isOrigin ? "A" : "B",
+    map: map
+  });
+  var contentString = '<div class="event-title">' +
+                  '<a href = "/events/' + event['id'] + '">' + event['title'] + '</a>' +
+                  (isOrigin ? "..." : "") +
+                  '<p>' + event['description'] + '</p>'
+                  '</div>';
   var infowindow = new google.maps.InfoWindow({
     content: contentString,
     maxWidth: 300
   });
-
-  function renderDirections(result) {
-    var directionsRenderer = new google.maps.DirectionsRenderer({
-      polylineOptions: {
-        strokeColor: "red"
-      },
-      suppressMarkers:true
-    });
-    directionsRenderer.setMap(map);
-    directionsRenderer.setDirections(result);
-  }
-
-  var directionsService = new google.maps.DirectionsService;
-  function requestDirections(start, end) {
-    directionsService.route({
-      origin: start,
-      destination: end,
-      travelMode: google.maps.DirectionsTravelMode.DRIVING
-    }, function(result) {
-      renderDirections(result);
-    });
-  }
-// requestDirections({lat: 49.28126719999999, lng: -123.0769687}, {lat: 49.2821004, lng: -123.1082745});
-// requestDirections('1833 Frances St. Vancouver, BC', '600 Hastings St. Vancouver, BC');
-  if (data) {
-    for(i = 0; i < data.length; i++) {
-
-      var markerA = new google.maps.Marker({
-        position: {lat: data[i]["lat"], lng: data[i]["lng"]},
-        title: "Origin",
-        label: "A",
-        map: map
-      });
-      var markerB = new google.maps.Marker({
-        position: {lat: data[i]["end_lat"], lng: data[i]["end_lng"]},
-        title: "Destination",
-        label: "B",
-        map: map
-      });
-
-      contentString = '<div class="event-title">' +
-                      '<a href = "/events/' + data[i]['id'] + '">' + data[i]['title'] + '</a>' +
-                      '<p>' + data[i]['description'] + '</p>'
-                      '</div>';
-
-      requestDirections({lat: data[i]["lat"], lng: data[i]["lng"]}, {lat: data[i]["end_lat"], lng: data[i]["end_lng"]});
-
-      makeInfoWindowEvent(map, infowindow, contentString, markerA, markerB);
-    }
-  }
-}
-function makeInfoWindowEvent(map, infowindow, contentString, markerA, markerB) {
-  google.maps.event.addListener(markerA, 'click', function() {
-    infowindow.setContent(contentString);
-    infowindow.open(map, markerA);
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.open(map, marker);
   });
-    google.maps.event.addListener(markerB, 'click', function() {
-    infowindow.setContent(contentString);
-    infowindow.open(map, markerB);
-  });
+  return marker;
 }
+
+// function makeInfoWindowEvent(map, infowindow, contentString, markerA, markerB) {
+//   google.maps.event.addListener(markerA, 'click', function() {
+//     infowindow.setContent(contentString);
+//     infowindow.open(map, markerA);
+//   });
+//     google.maps.event.addListener(markerB, 'click', function() {
+//     infowindow.setContent(contentString);
+//     infowindow.open(map, markerB);
+//   });
+// }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170303003613) do
+ActiveRecord::Schema.define(version: 20170303013659) do
 
   create_table "events", force: :cascade do |t|
     t.string   "title"
@@ -19,15 +19,24 @@ ActiveRecord::Schema.define(version: 20170303003613) do
     t.text     "description"
     t.boolean  "status"
     t.integer  "user_id"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
     t.datetime "scheduled_at"
     t.string   "address"
     t.string   "city"
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
+    t.datetime "photo_updated_at"
     t.string   "end_address"
     t.float    "end_lat"
     t.float    "end_lng"
     t.index ["user_id"], name: "index_events_on_user_id"
+  end
+
+  create_table "events_users", id: false, force: :cascade do |t|
+    t.integer "user_id",  null: false
+    t.integer "event_id", null: false
   end
 
   create_table "helper_lists", force: :cascade do |t|
@@ -72,9 +81,13 @@ ActiveRecord::Schema.define(version: 20170303003613) do
     t.string   "image"
     t.text     "description"
     t.date     "DoB"
-    t.integer  "phone_number"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.string   "phone_number"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
+    t.datetime "photo_updated_at"
   end
 
 end
