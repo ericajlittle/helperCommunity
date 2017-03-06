31761ae0e7c7626b319cc832dc90f06ab424355e
MichaelMini <mcho911@gmail.com>
2017-03-06 21:18:58 +0000

    import AllEvents component

--- client/app/bundles/HelperApp/startup/registration.jsx
+++ client/app/bundles/HelperApp/startup/registration.jsx
@@ -2,8 +2,9 @@ import ReactOnRails from 'react-on-rails';
 
 import App from '../components/App';
 import NewEvent from '../components/NewEvent';
+import AllEvents from '../components/AllEvents';
 
 // This is how react_on_rails can see the HelloWorld in the browser.
 ReactOnRails.register({
-  App, NewEvent
+  App, NewEvent, AllEvents
 });
