68d37a1a06e1f6e1fe747807a0033faae750c3bd
MichaelMini <mcho911@gmail.com>
2017-03-07 22:15:57 +0000

    Disable submit button when input is empty.

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -48,4 +48,31 @@ $(function() {
   // var mapHeight = $(window).height();
   // $("#map").css('height', height);
 
+  // Validation and disable review submit button
+  var $input = $('#review_content'),
+      $select = $('#review_rating'),
+      $register = $(':submit');
+  $register.attr('disabled', true);
+
+  function validateInputs() {
+    return ($input.val().length > 0) && ($select.find(':selected').val().length > 0)
+  }
+
+  function validateSubmit() {
+    if (validateInputs()) {
+      $register.attr('disabled', false);
+    } else {
+      $register.attr('disabled', true);
+    }
+  }
+
+  $input.on('blur input', function() {
+    validateSubmit();
+  })
+
+  $select.on('change', function() {
+    validateSubmit();
+  })
+
+
 });
